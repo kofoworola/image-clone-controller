97b38959f74cb8bae8d98783c1d782808b25347a
Kofo Okesola <okesolakofo@gmail.com>
2021-03-22 22:25:13 +0100

    properly backs up images

--- go.mod
+++ go.mod
@@ -3,6 +3,17 @@ module github.com/kofoworola/image-clone-controller
 go 1.14
 
 require (
+	github.com/google/go-containerregistry v0.4.1
+	github.com/maxbrunsfeld/counterfeiter/v6 v6.3.0 // indirect
+	go.uber.org/zap v1.15.0
+	golang.org/x/mod v0.4.2 // indirect
+	golang.org/x/sys v0.0.0-20210320140829-1e4c9ba3b0c4 // indirect
+	golang.org/x/tools v0.1.0 // indirect
+	google.golang.org/genproto v0.0.0-20201110150050-8816d57aaa9a
 	k8s.io/api v0.20.5
+	k8s.io/apimachinery v0.20.5
+	k8s.io/code-generator v0.20.5 // indirect
+	k8s.io/gengo v0.0.0-20210203185629-de9496dff47b // indirect
+	k8s.io/klog/v2 v2.8.0 // indirect
 	sigs.k8s.io/controller-runtime v0.8.3
 )
--- main.go
+++ main.go
@@ -3,15 +3,22 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
 	"os"
 	"strings"
 
+	"github.com/google/go-containerregistry/pkg/authn"
+	"github.com/google/go-containerregistry/pkg/name"
+	"github.com/google/go-containerregistry/pkg/v1/remote"
 	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/config"
 	"sigs.k8s.io/controller-runtime/pkg/controller"
 	"sigs.k8s.io/controller-runtime/pkg/handler"
 	"sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 	"sigs.k8s.io/controller-runtime/pkg/manager/signals"
 	"sigs.k8s.io/controller-runtime/pkg/predicate"
@@ -24,19 +31,24 @@ import (
 var (
 	registry       string
 	skipNamespaces string
+	username       string
+	password       string
 
 	ignoreNs = []string{"kube-system"}
 )
 
 func init() {
-	flag.StringVar(&registry, "registry", "", "what registry will be used to make backups")
+	flag.StringVar(&registry, "registry", "kofoworola", "what registry will be used to make backups without the forward slash")
 	flag.StringVar(
 		&skipNamespaces,
 		"ignore-namespaces",
 		"",
 		"comma separated list of namespaces to ignore. NOTE `kube-system` namesapces will always be ignored",
 	)
+	flag.StringVar(&username, "username", "kofoworola", "registry username")
+	flag.StringVar(&password, "password", "", "registry password")
 
+	log.SetLogger(zap.New())
 }
 func main() {
 	flag.Parse()
@@ -110,8 +122,97 @@ type imageReconciler struct {
 	client client.Client
 }
 
-func (r imageReconciler) Reconcile(ctx context.Context, req reconcile.Request) (reconcile.Result, error) {
+func (r *imageReconciler) Reconcile(ctx context.Context, req reconcile.Request) (reconcile.Result, error) {
 	logger := log.FromContext(ctx)
-	logger.Info("received reconcile request")
+	deploy := &appsv1.Deployment{}
+	daemonSet := &appsv1.DaemonSet{}
+	isDaemon := false
+	err := r.client.Get(ctx, req.NamespacedName, deploy)
+	if errors.IsNotFound(err) {
+		isDaemon = true
+		logger.Info("is not deployment, checking daemon")
+		if err = r.client.Get(ctx, req.NamespacedName, daemonSet); errors.IsNotFound(err) {
+			logger.Error(err, "not found")
+			return reconcile.Result{}, nil
+		}
+
+	}
+
+	if isDaemon {
+		conts := daemonSet.Spec.Template.Spec.Containers
+		modifiedContainers := make([]corev1.Container, len(conts))
+		for i, container := range conts {
+			newImage, err := r.backupImage(container.Image, ctx)
+			if err != nil {
+				logger.Error(err, "error backing up image", "image-name", container.Image)
+				return reconcile.Result{}, nil
+			}
+			container.Image = newImage
+			modifiedContainers[i] = container
+		}
+		daemonSet.Spec.Template.Spec.Containers = modifiedContainers
+		if err := r.client.Update(ctx, daemonSet); err != nil {
+			logger.Error(err, "error updating image")
+		}
+	} else {
+		conts := deploy.Spec.Template.Spec.Containers
+		modifiedContainers := make([]corev1.Container, len(conts))
+		for i, container := range conts {
+			newImage, err := r.backupImage(container.Image, ctx)
+			if err != nil {
+				logger.Error(err, "error backing up image", "image-name", container.Image)
+				return reconcile.Result{}, nil
+			}
+			container.Image = newImage
+			modifiedContainers[i] = container
+		}
+		deploy.Spec.Template.Spec.Containers = modifiedContainers
+		if err := r.client.Update(ctx, deploy); err != nil {
+			logger.Error(err, "error updating image")
+		}
+
+	}
 	return reconcile.Result{}, nil
+}
+
+func (r *imageReconciler) backupImage(imageName string, ctx context.Context) (string, error) {
+	logger := log.FromContext(ctx)
+
+	// check if its a part of the registry already
+	if strings.Contains(imageName, registry+"/") {
+		return imageName, nil
+	}
+
+	reference, err := name.ParseReference(imageName)
+	if err != nil {
+		return "", fmt.Errorf("error parsing image reference: %w", err)
+	}
+
+	image, err := remote.Image(reference)
+	if err != nil {
+		return "", fmt.Errorf("error getting image: %w", err)
+	}
+
+	var newImageName string
+	index := strings.LastIndex(imageName, "/")
+	if index == -1 {
+		newImageName = fmt.Sprintf("%s/%s", registry, imageName)
+	} else {
+		newImageName = fmt.Sprintf("%s/%s", registry, imageName[index+1:])
+	}
+
+	logger.Info("pushing to new registry", "image-name", newImageName)
+	reference, err = name.ParseReference(newImageName)
+	if err != nil {
+		return "", fmt.Errorf("error creating reference for %s: %v", newImageName, err)
+	}
+
+	if err := remote.Write(reference, image, remote.WithAuth(&authn.Basic{
+		Username: username,
+		Password: password,
+	})); err != nil {
+		return "", fmt.Errorf("error writing to new registry %s: %v", newImageName, err)
+	}
+
+	return newImageName, nil
 }
